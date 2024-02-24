a249d3987bf0057d6eba7dabce0f87fb5a8732aa
Jacob Huynh <jacobshuynh2@gmail.com>
2024-02-24 18:49:04 -0500

    user landing page (basics: displays username and name)

--- whistleblowingsite/login/urls.py
+++ whistleblowingsite/login/urls.py
@@ -4,7 +4,9 @@ from django.views.generic import TemplateView
 from django.contrib.auth.views import LogoutView
 
 
+
 urlpatterns = [
     path("", TemplateView.as_view(template_name="index.html")),
-    path('logout', LogoutView.as_view()),
+    path('logout/', LogoutView.as_view()),
+    path('userlanding/<str:username>/', views.user_landing_view),
 ]
