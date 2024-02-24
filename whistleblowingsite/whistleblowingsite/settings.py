0118ba0f9b6711d82b29b53ae6de1556a6d80ab9
blairezh <113472440+blairezh@users.noreply.github.com>
2024-02-24 11:31:15 -0500

    static file configuration in settings.py

--- whistleblowingsite/whistleblowingsite/settings.py
+++ whistleblowingsite/whistleblowingsite/settings.py
@@ -9,6 +9,9 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
+import django_heroku
+import dj_database_url
 
 from pathlib import Path
 
@@ -147,3 +150,7 @@ AUTHENTICATION_BACKENDS = (
 
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
+
+#configure static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
