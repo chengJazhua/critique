4d4c370b3072e06c5d3f3d1071234975a7b26f60
Alisha Qian <sta2fu@virginia.edu>
2024-02-25 04:21:10 -0500

    made google auth redirecting method

--- login/views.py
+++ login/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
-
+from django.contrib.auth.signals import user_logged_in
+from django.dispatch import receiver
 # Create your views here.
+
+@receiver(user_logged_in)
+def redirect_after_google_login(sender, request, user, **kwargs):
+    print(f"Signal received from {sender}")
+    # Check if the user is in the "site admin" group
+    if user.groups.filter(name='site admin').exists():
+        print(f"I am an admin")
+        # Redirect to the admin landing page
+        return redirect('adminlanding/')
+    else:
+        print(f"I am a user")
+        # Redirect to the user landing page
+        return redirect('userlanding/')
+
 def home(request):
     return render(request, "login/home.html") #issue here
 
