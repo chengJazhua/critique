cbc29cd5474dedf8fdb55d2484b61b8544f14799
chengJazhua <chengjazhua@gmail.com>
2024-02-24 21:41:00 -0500

    Integrate google login with home page

--- whistleblowingsite/login/urls.py
+++ whistleblowingsite/login/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="index.html")),
+    path("", TemplateView.as_view(template_name="home.html")),
     path('logout/', LogoutView.as_view()),
     path('userlanding/<str:username>/', views.user_landing_view),
 ]
