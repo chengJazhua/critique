97ffea4a13f75a8570dde0de4a6374b1e2b46c7f
chengJazhua <chengjazhua@gmail.com>
2024-03-17 05:11:52 -0400

    pray

--- whistleblowingsite/settings.py
+++ whistleblowingsite/settings.py
@@ -203,4 +203,4 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-django_heroku.settings(locals(), staticfiles=False)
+django_heroku.settings(locals(), staticfiles=False, test_runner=False)
