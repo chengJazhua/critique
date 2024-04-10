615f406f6988d42fc71403f8c8df42210a3e1de2
chengJazhua <chengjazhua@gmail.com>
2024-04-09 20:06:58 -0400

    model additions

--- login/forms.py
+++ login/forms.py
@@ -4,4 +4,5 @@ from login.models import Report
 class ReportForm(forms.ModelForm):
     class Meta:
         model = Report
-        fields=['userID','className', 'professorName', 'studentName', 'rating','workType','fileLink','status','feedback']
+        fields=['userID', 'report', 'className', 'professorName', 'studentName', 
+                'rating','workType','fileLink','status','feedback', 'professor_email', 'email_prof', 'private', 'reported']
